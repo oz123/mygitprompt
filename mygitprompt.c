63e088249fd0dea76644e880f4b89346d36fc298
Oz Tiram <oz.tiram@gmail.com>
2023-12-29 22:47:08 +0100

    Read git status using popen

--- mygitprompt.c
+++ mygitprompt.c
@@ -1,10 +1,28 @@
-#include <stdio.h>
-
 //
 // main is where all program execution starts
 //
-int main(int argc, char **argv) {
-  printf("Hello there.\n");
-  return 0;
-}
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+
+    fp = popen("git status", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        return 1;
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("%s", line);
+    }
+
+    pclose(fp);
+    if (line)
+        free(line);
+
+    return 0;
+}
