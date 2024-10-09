35fe14354d19248e068e2965a010a84f873ee1c8
Oz Tiram <oz.tiram@gmail.com>
2024-10-09 22:44:30 +0200

    Make tests robuster

--- test.c
+++ test.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <string.h>
 #include "gitstatus.h"
 
 int main(void) {
@@ -5,29 +7,58 @@ int main(void) {
 
     char line1[] = "## main...origin/main [ahead 9, behind 14]";
     char line2[] = "## test...origin/test [ahead 5]";
-    char line3[] = "## branch...origin/branch [behind 20]";
-    char line4[] = "## branch2...origin/branch2";
+    char line3[] = "## branch...mine/branch [behind 20]";
+    char line4[] = "## branch2...upstream/branch2";
 
     GitStatus *gs = new_gitstatus();
+
     parse_branch_name(line1, gs);
+    parse_remote_name(line1, gs);
     parse_ahead_behind(line1, gs);
-    printf("Branch: %s, Ahead: %d, Behind: %d\n", gs->branch_name, gs->ahead_count, gs->behind_count);
-    gs->ahead_count=0;
-    gs->behind_count=0;
-    printf("Test 1 passed");
-    parse_ahead_behind(line2, gs);
-    printf("Branch: %s, Ahead: %d, Behind: %d\n", gs->branch_name, gs->ahead_count, gs->behind_count);
-    gs->ahead_count=0;
-    gs->behind_count=0;
-    printf("Test 2 passed");
-    parse_ahead_behind(line3, gs);
-    printf("Branch: %s, Ahead: %d, Behind: %d\n", gs->branch_name, gs->ahead_count, gs->behind_count);
-    gs->ahead_count=0;
-    gs->behind_count=0;
-    printf("Test 3 passed");
-    parse_ahead_behind(line4, gs);
-    printf("Branch: %s, Ahead: %d, Behind: %d\n", gs->branch_name, gs->ahead_count, gs->behind_count);
-    printf("Test 4 passed\n");
+
+    assert(!strcmp("main", gs->branch_name));
+    assert(!strcmp("origin", gs->remote_name));
+    assert(gs->ahead_count == 9);
+    assert(gs->behind_count == 14);
     free_gitstatus(gs);
+    printf("Test 1 passed\n");
+
+    gs = new_gitstatus();
+    parse_branch_name(line2, gs);
+    parse_remote_name(line2, gs);
+    parse_ahead_behind(line2, gs);
+
+    assert(!strcmp("test", gs->branch_name));
+    assert(!strcmp("origin", gs->remote_name));
+    assert(gs->ahead_count == 5);
+    assert(gs->behind_count == 0);
+
+    free_gitstatus(gs);
+    printf("Test 2 passed\n");
+
+    gs = new_gitstatus();
+    parse_branch_name(line3, gs);
+    parse_remote_name(line3, gs);
+    parse_ahead_behind(line3, gs);
+
+    assert(!strcmp("branch", gs->branch_name));
+    assert(!strcmp("mine", gs->remote_name));
+    assert(gs->ahead_count == 0);
+    assert(gs->behind_count == 20);
+    free_gitstatus(gs);
+    printf("Test 3 passed\n");
+
+    gs = new_gitstatus();
+    parse_branch_name(line4, gs);
+    parse_remote_name(line4, gs);
+    parse_ahead_behind(line4, gs);
+
+    assert(!strcmp("branch2", gs->branch_name));
+    assert(!strcmp("upstream", gs->remote_name));
+    assert(gs->ahead_count == 0);
+    assert(gs->behind_count == 0);
+
+    free_gitstatus(gs);
+    printf("Test 4 passed\n");
     return 0;
 }
