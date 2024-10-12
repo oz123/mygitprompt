049bc724cc381ee40484f559e5f9ff34715336c0
Oz Tiram <oz.tiram@gmail.com>
2024-10-12 09:20:57 +0200

    Add license header

--- mygitprompt.c
+++ mygitprompt.c
@@ -1,6 +1,21 @@
-//
-// main is where all program execution starts
-//
+/* 
+ This file is part of mygitprompt.
+
+ mygitprompt is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License, version 3 or later
+ as published by the Free Software Foundation;
+
+ mygitprompt is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with mygitprompot; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ Copyright (C) 2024 Oz N Tiram <oz.tiram@gmail.com>
+*/
 
 #define _GNU_SOURCE
 #include <stdio.h>
